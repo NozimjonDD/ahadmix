7a76a073e051f2ae59213cfa38e42be3ff2024b5
anot-ubuntu <nozimfromandijan@gmail.com>
2026-05-31 22:45:46 +0500

    first commit

--- ahadmix/urls.py
+++ ahadmix/urls.py
@@ -1,21 +1,26 @@
-"""ahadmix URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.static import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('common.urls', namespace='common')),
 ]
+
+if settings.DEBUG:
+    docs_root = settings.BASE_DIR / 'docs'
+    urlpatterns += [
+        path(
+            'videos/<path:path>',
+            static_serve,
+            {'document_root': docs_root / 'videos'},
+        ),
+        path(
+            'AHADMIX-KP-2026.pdf',
+            static_serve,
+            {'path': 'AHADMIX-KP-2026.pdf', 'document_root': docs_root},
+        ),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
