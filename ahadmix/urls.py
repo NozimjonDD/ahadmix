690a79de084ba2dd0c2120ae5fdf1e726a7a7adf
anot-ubuntu <nozimfromandijan@gmail.com>
2026-05-31 23:15:19 +0500

    first commit

--- ahadmix/urls.py
+++ ahadmix/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.static import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,17 +9,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    docs_root = settings.BASE_DIR / 'docs'
-    urlpatterns += [
-        path(
-            'videos/<path:path>',
-            static_serve,
-            {'document_root': docs_root / 'videos'},
-        ),
-        path(
-            'AHADMIX-KP-2026.pdf',
-            static_serve,
-            {'path': 'AHADMIX-KP-2026.pdf', 'document_root': docs_root},
-        ),
-    ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
