96343bb7cacc8d1508248e9ddb9141cac53a337d
rentmi <nozimfromandijan@gmail.com>
2026-06-30 17:38:21 +0500

    API added

--- api/admin/urls.py
+++ api/admin/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from api.admin.views import *
 
 urlpatterns = [
+    # FAQ
     path('faq/list/', AdminFAQListAPIView.as_view()),
     path('create/', AdminFAQCreateAPIView.as_view()),
+
+    # MONITOR
+    # path('manitor/list/', Adm????tAPIView.as_view()),
+
+
 ]
